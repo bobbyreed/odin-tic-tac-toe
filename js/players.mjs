b715ade9e9147e812e28c8a148c2b6ac0e258bf2
bobby <37113385+bobbyreed@users.noreply.github.com>
2026-01-24 13:52:30 -0600

    Implements setting player name and mark in GUI

--- js/players.mjs
+++ js/players.mjs
@@ -8,3 +8,12 @@ const player2 = new Player(prompt("Enter name for Player 2:", "Player 2"), promp
 
 console.log(`${player1.name} will use ${player1.mark} as mark`);
 console.log(`${player2.name} will use ${player2.mark} as mark`);
+
+const p1name = document.getElementById('p1-name');
+p1name.innerText = player1.name;
+const p1mark = document.getElementById('p1-mark');
+p1mark.innerText = player1.mark;
+const p2name = document.getElementById('p2-name');
+p2name.innerText = player2.name;
+const p2mark = document.getElementById('p2-mark');
+p2mark.innerText = player2.mark;
